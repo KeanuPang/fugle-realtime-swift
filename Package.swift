1b54ac582721637b8f64223ceb40b3dc2509e5ab
KeanuPang <keanu.pang@gmail.com>
2022-05-12 11:19:43 +0800

    Rename for library

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "fugle-realtime-swift",
+            name: "FugleRealtime",
             targets: ["fugle-realtime-swift"]),
     ],
     dependencies: [
