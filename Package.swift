e7a3331aa64aee1a08f23ab93ccd609eee9a376a
KeanuPang <keanu.pang@gmail.com>
2022-05-20 17:32:18 +0800

    Update `Package.swift`, add support of iOS platform

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "fugle-realtime-swift",
     platforms: [
-        .macOS(.v10_15)
+        .macOS(.v10_15), .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
